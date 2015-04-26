df9fb26d5bc507c5ce9f98e14590295358a698a5
Quyse Lert <quyse0@gmail.com>
2015-04-26 22:08:56 +0100

    fixes for latest flaw

--- Exporter.hs
+++ Exporter.hs
@@ -4,6 +4,8 @@ import Flaw.Asset.Collada
 
 import Control.Monad.IO.Class
 import Data.Word
+import qualified Data.Vector.Generic as VG
+import qualified Data.Vector.Unboxed as VU
 import Foreign.Storable
 import Language.Haskell.TH
 
@@ -25,16 +27,16 @@ main = do
 		parseGeometry vertexConstructor =<< getElementById elementId
 	case eitherGeometry of
 		Right rawVertices -> do
-			let (vertices, indices) = indexVertices rawVertices
-			verticesBytes <- packList vertices
+			(vertices, indices) <- indexVertices rawVertices
+			verticesBytes <- packVector vertices
 			(isIndices32Bit, indicesBytes) <- do
 				if length vertices > 0x10000 then do
-					indicesBytes <- packList ((map fromIntegral indices) :: [Word32])
+					indicesBytes <- packVector ((VG.map fromIntegral indices) :: VU.Vector Word32)
 					return (True, indicesBytes)
 				else do
-					indicesBytes <- packList ((map fromIntegral indices) :: [Word16])
+					indicesBytes <- packVector ((VG.map fromIntegral indices) :: VU.Vector Word16)
 					return (False, indicesBytes)
-			putStrLn $ show (length indices, isIndices32Bit)
+			putStrLn $ show (VG.length indices, isIndices32Bit)
 			BL.writeFile "assets/field.vertices" $ BL.fromStrict verticesBytes
 			BL.writeFile "assets/field.indices" $ BL.fromStrict indicesBytes
 		Left s -> fail s
