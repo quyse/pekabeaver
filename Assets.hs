703d2eae11956c27a6fc669624890d6b10309c48
Quyse Lert <quyse0@gmail.com>
2016-03-03 18:53:38 +0300

    trying to deploy on gitlab pages

--- Assets.hs
+++ Assets.hs
@@ -20,5 +20,10 @@ do
 	addAsset "castle.jpg" =<< emitPlainTextureAsset "assets/images/0_castle.jpg"
 	addAsset "beaver.jpg" =<< emitPlainTextureAsset "assets/images/0_beaver.jpg"
 	addAsset "peka.png" =<< emitPlainTextureAsset "assets/images/0_peka0.png"
+	addAsset "beaver_icon.png" =<< emitPlainTextureAsset "assets/images/beaver_icon.png"
+	addAsset "peka_icon.png" =<< emitPlainTextureAsset "assets/images/peka_icon.png"
+	addAsset "beaver_win.jpg" =<< emitPlainTextureAsset "assets/images/beaver_win.jpg"
+	addAsset "peka_win.jpg" =<< emitPlainTextureAsset "assets/images/peka_win.jpeg"
+	addAsset "styles.css" =<< emitPlainTextureAsset "assets/styles.css"
 
 	return []
